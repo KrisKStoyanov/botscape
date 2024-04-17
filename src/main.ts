71734d05d26e9beaf9b7d86bbb6d715f17a09074
KrisWooDoo <kristiyan.stoyanov.krs@gmail.com>
2024-04-17 21:10:25 +0300

    added scripts folder for custom classes and implemented game button functionality separately

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,8 @@
 import 'phaser';
+import GameButton from './scripts/game-button';
 
 const screenWidth = window.screen.width * window.devicePixelRatio;
 const screenHeight = window.screen.height * window.devicePixelRatio;
-
-class GameButton extends Phaser.GameObjects.Image
-{
-    constructor(scene: Phaser.Scene, x: number, y: number, texture: string, callback: Function)
-    {
-        super(scene, x, y, texture);
-
-        this.setInteractive({useHandCursor: true});
-        this.on('pointerover', () => this.enterButtonHoverState());
-        this.on('pointerout', () => this.enterButtonRestState());
-        this.on('pointerdown', () => this.enterButtonActiveState());
-        this.on('pointerup', () => 
-            {
-                this.enterButtonHoverState();
-                callback();
-            });
-    }
-
-    enterButtonHoverState(): void
-    {
-        this.setScale(0.9);
-    }
-    enterButtonRestState(): void
-    {
-        this.setScale(1.0);
-    }
-    enterButtonActiveState(): void
-    {
-        this.setScale(0.8);
-    }
-}
 
 class PlayGame extends Phaser.Scene 
 {
@@ -239,8 +209,6 @@ class PlayGame extends Phaser.Scene
         this.helpText.setVisible(false);
         this.helpText.setActive(false);
 
-        // this.startButton = new Phaser.GameObjects.Image(this, 400, 300, 'start-button');
-        // this.add.existing(this.startButton);
         this.startButton = new GameButton(this, screenCenterX, screenCenterY, 'start-button', () => this.startGame());
         this.add.existing(this.startButton);
         
